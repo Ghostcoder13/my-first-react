67db39c95c94d3ddf46453ab030689099beb8f55
Ghostcoder13 <sou3sou@hotmail.fr>
2023-03-28 12:11:57 +0200

    exo quetes 3 react

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Bulbasaur from './components/PokemonCard/card'
+import Bulbasaur from './components/PokemonCard/Pokemoncard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,5 +14,7 @@ function App() {
     </div>
   )
 }
+
+
 
 export default App
---
+++ src/components/PokemonCard/Pokemoncard.jsx
@@ -0,0 +1,23 @@
+const pokemonList = [
+   {
+     name: "bulbasaur",
+     imgSrc:
+       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+   },
+   {
+     name: "mew",
+   },
+ ];
+
+function Bulbasaur() {
+   let pokemon = pokemonList[1]
+   return (
+   <figure>
+      {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc}
+         alt={pokemon.name}/>}
+    <figcaption>{pokemon.name}</figcaption>
+    </figure>
+   );
+}
+
+export default Bulbasaur;
